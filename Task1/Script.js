bcda5df1219b0b2254ab457a6a24b1204b003cb6
Даниил Рудь <ruddanil99@gmail.com>
2022-03-30 15:30:33 +0400

    Improvements

--- Task1/Script.js
+++ Task1/Script.js
@@ -92,35 +92,98 @@ const example4 = () => {
     timer.style.display = "flex";
 };
 
-let time = document.getElementById("time");
-let pause = false;
-let sec = 0;
-let min = 0;
-let pauseTime;
 
-const timerLogic = () => {
-    if (!pause) {
-        sec++;
-        if (sec < 10) sec = "0" + sec;
-        if (sec == 60) {
-            min++;
-            sec = 0;
+// let time = document.getElementById("time");
+// let pause = false;
+// let sec = 0;
+// let min = 0;
+// let pauseTime;
+//
+// const timerLogic = () => {
+//     let seconds = parseInt(prompt("Введите количество секунд: "), 10);
+//     let minutes = 0;
+//     while (isNaN(seconds) || seconds <= 0) {
+//         alert("Некорректные данные");
+//         seconds = parseInt(prompt("Введите количество секунд: "), 10);
+//     }
+//     if(seconds >= 60*60) {
+//         minutes = 60*60/seconds;
+//     }
+//     if (!pause) {
+//         sec--;
+//         if (sec < 10) sec = "0" + sec;
+//         if (sec == 60) {
+//             min++;
+//             sec = 0;
+//         }
+//         time.innerHTML = min + ":" + sec;
+//     } else {
+//         return;
+//     }
+// };
+// const startTimer = () => {
+//     sec = 0;
+//     min = 0;
+//     pause = false;
+//     clearInterval(pauseTime);
+//     pauseTime = setInterval(timerLogic, 1000);
+// };
+// const stopTimer = () => {
+//     pause = true;
+//     clearInterval(pauseTime);
+// };
+let count4 = 0;
+const timer = () => {
+    if(count4==0){
+
+        document.getElementById("text").style.display = "inline";
+        document.getElementById("b7").style.display = "inline";
+        document.getElementById("b8").style.display = "inline";
+        document.getElementById("time").style.display = "block";
+        document.getElementById("timer").style.display = "block";
+        let buttonRun = document.getElementById("b7")
+        let buttonStop = document.getElementById("b8")
+        let timerShow = document.getElementById("time");
+
+        function Timer(){
+            var timerInput = document.getElementById("text");
+            timerInput.value = parseInt(timerInput.value)-1;
+            var seconds = timerInput.value%60;
+            var minutes = timerInput.value/60%60;
+            var hour = timerInput.value/60/60%60;
+            if (timerInput.value <= 0) {
+                window.clearInterval(window.TimerId);
+                alert("Время закончилось");
+            } else {
+                let strTimer = `${Math.trunc(hour)}:${Math.trunc(minutes)}:${seconds}`;
+                timerShow.innerHTML = strTimer;
+            }
+
         }
-        time.innerHTML = min + ":" + sec;
-    } else {
-        return;
+
+        buttonRun.onclick =  function() {
+            window.TimerId = window.setInterval(Timer, 1000);
+        }
+
+        buttonStop.onclick =  function() {
+            window.clearInterval(window.TimerId);
+        }
+
+        count4 = 1;
+
     }
-};
-const startTimer = () => {
-    sec = 0;
-    min = 0;
-    pause = false;
-    clearInterval(pauseTime);
-    pauseTime = setInterval(timerLogic, 1000);
-};
-const stopTimer = () => {
-    pause = true;
-    clearInterval(pauseTime);
+
+    else{
+
+        document.getElementById("text").style.display = "none";
+        document.getElementById("b7").style.display = "none";
+        document.getElementById("b8").style.display = "none";
+        document.getElementById("time").style.display = "none";
+        document.getElementById("timer").style.display = "none";
+        count4 = 0;
+
+    }
+
 };
 
 let opac = 0;
@@ -148,14 +211,36 @@ const example5 = () => {
             if (k === 100) img.style.display = "none";
         }, 20);
     });
-
     const timerI = setInterval(() => {
         k--;
         opac = 100 - k;
         img.style.opacity = opac / 100;
         if (k <= 0 || opac >= 100) clearInterval(timerI);
     }, 20);
-
     k = 100;
     ii = 0;
+};
+let countQuestion = 10;
+const checkTest = () => {
+    let scorePlace = document.getElementById("scoreTest");
+    let score = 0;
+    for (let i = 1; i < countQuestion + 1; i++) {
+        let ans = document.getElementsByName("q" + i);
+        let el = document.getElementById("corrAns" + i);
+
+        if (ans[0].checked && ans[0].value == 1) {
+            score++;
+            el.parentNode.style.backgroundColor = "green";
+        } else if (ans[0].checked) el.parentNode.style.backgroundColor = "red";
+        if (ans[1].checked && ans[1].value == 1) {
+            score++;
+            el.parentNode.style.backgroundColor = "green";
+        } else if (ans[1].checked) el.parentNode.style.backgroundColor = "red";
+        if (ans[2].checked && ans[2].value == 1) {
+            score++;
+            el.parentNode.style.backgroundColor = "green";
+        } else if (ans[2].checked) el.parentNode.style.backgroundColor = "red";
+    }
+
+    scorePlace.innerText = "Результат: " + score + "/10";
 };
